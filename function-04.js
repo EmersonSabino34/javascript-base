bfb5c04b13a42578f11baef9973e3e0e8ff4fbcd
EmersonSabino34 <168974048+EmersonSabino34@users.noreply.github.com>
2024-11-03 18:54:15 -0300

    Create function-04.js

---
+++ function-04.js
@@ -0,0 +1,14 @@
+let valor1 = 20;
+let valor2 = 20;
+
+function somar(valor1, valor2){
+  let total = valor1 + valor2
+
+  consolet(total)
+}
+
+const somar2 = (valor1, valor2) => {
+  let total = valor1 + valor2
+
+  consolet(total)
+}
